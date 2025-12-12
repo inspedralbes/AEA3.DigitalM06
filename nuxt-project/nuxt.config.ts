6a110cebcfdf512aad6844715014da05cb56b579
happybololo <poldiazb@gmail.com>
2025-12-12 09:58:35 +0100

    arreglo

--- nuxt-project/nuxt.config.ts
+++ nuxt-project/nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
-  css: ['~/assets/main.css'],
+  css: ['./assets/main.css'],
+  alias: {
+    '~': '.'
+  },
   runtimeConfig: {
     public: {
       apiBaseUrl: 'https://api.jikan.moe/v4'
