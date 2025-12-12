96f19cb99f5bb7f629ef81e675973ae0cf2d8381
happybololo <poldiazb@gmail.com>
2025-12-12 10:06:19 +0100

    Fix: remove invalid prerender routes

--- nuxt-project/nuxt.config.ts
+++ nuxt-project/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: ['/sitemap.xml', '/robots.txt']
+      routes: ['/']
     }
   },
   runtimeConfig: {
